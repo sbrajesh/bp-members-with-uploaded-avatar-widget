534f78c0cefaee55b74fa90ecf987e36afc2451d
Brajesh Singh <brajesh@cosmiccoders.com>
2017-10-15 21:16:16 +0530

    Add text domain in the strings

--- bp-member-with-uploaded-avatar-widget.php
+++ bp-member-with-uploaded-avatar-widget.php
@@ -12,7 +12,7 @@ class BP_Members_With_Uploaded_Avatar_Widget extends WP_Widget {
 	 * Constructor.
 	 */
 	public function __construct() {
-		parent::__construct( false, __( 'Members With Uploaded Avatars' ) );
+		parent::__construct( false, __( 'Members With Uploaded Avatars', 'bp-members-with-uploaded-avatar-widget' ) );
 	}
 
 	/**
@@ -66,7 +66,7 @@ class BP_Members_With_Uploaded_Avatar_Widget extends WP_Widget {
 	public function form( $instance ) {
 
 		$default = array(
-			'title'  => __( 'Recent Members' ),
+			'title'  => __( 'Recent Members', 'bp-members-with-uploaded-avatar-widget' ),
 			'type'   => 'random',
 			'max'    => 5,
 			'size'   => 'full',
@@ -77,42 +77,39 @@ class BP_Members_With_Uploaded_Avatar_Widget extends WP_Widget {
 		$instance = (array) $instance;// type cast to array.
 
 		$instance = wp_parse_args( $instance, $default );
-		extract( $instance );
 
 		$max           = absint( $instance['max'] );
 		$title         = esc_html( $instance['title'] );
 		$type          = $instance['type'];
-		$avatar_option = $instance['avatar_option'];
+		$avatar_option = isset( $instance['avatar_option'] ) ? $instance['avatar_option'] : 0;
 		$size          = $instance['size'];
 		$width         = $instance['width'];
 		$height        = $instance['height'];
 		?>
 
 		<p>
-			<label for="bp-member-with-avatar-title"><strong><?php _e( 'Title:' ); ?> </strong>
+			<label for="bp-member-with-avatar-title"><strong><?php _e( 'Title:', 'bp-members-with-uploaded-avatar-widget' ); ?> </strong>
 				<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>"
 				       name="<?php echo $this->get_field_name( 'title' ); ?>" type="text"
 				       value="<?php echo esc_attr( $title ); ?>" style="width: 100%"/>
 			</label>
 		</p>
 		<p>
-			<label for="bp-member-with-avatar-max"><?php _e( 'Maximum no. of Users to show' ); ?>
+			<label for="bp-member-with-avatar-max"><?php _e( 'Maximum no. of Users to show', 'bp-members-with-uploaded-avatar-widget' ); ?>
 				<input class="widefat" id="<?php echo $this->get_field_id( 'max' ); ?>"
 				       name="<?php echo $this->get_field_name( 'max' ); ?>" type="text"
 				       value="<?php echo esc_attr( $max ); ?>" style="width: 30%"/>
 			</label>
 		</p>
 		<p>
-			<label for="bp-member-with-avatar-type"><?php _e( 'Order By' ); ?>
-				<select class="widefat" id="<?php echo $this->get_field_id( 'type' ); ?>"
-				        name="<?php echo $this->get_field_name( 'type' ); ?>" style="width: 30%">
-					<option value="random" <?php selected( $type, 'random' ); ?> >Random</option>
-					<option value="alphabetical" <?php selected( $type, 'alphabetical' ); ?> >Alphabetically</option>
-					<option value="active" <?php selected( $type, 'active' ); ?> >Active</option>
-					<option value="newest" <?php selected( $type, 'newest' ); ?> >Recently Joined</option>
-					<option value="popular" <?php selected( $type, 'popular' ); ?> >Popularity</option>
+			<label for="bp-member-with-avatar-type"><?php _e( 'Order By', 'bp-members-with-uploaded-avatar-widget' ); ?>
+				<select class="widefat" id="<?php echo $this->get_field_id( 'type' ); ?>" name="<?php echo $this->get_field_name( 'type' ); ?>" style="width: 30%">
+					<option value="random" <?php selected( $type, 'random' ); ?> ><?php _e( 'Random', 'bp-members-with-uploaded-avatar-widget' ); ?></option>
+					<option value="alphabetical" <?php selected( $type, 'alphabetical' ); ?> ><?php _e( 'Alphabetically', 'bp-members-with-uploaded-avatar-widget' ); ?></option>
+					<option value="active" <?php selected( $type, 'active' ); ?> ><?php _e( 'Active', 'bp-members-with-uploaded-avatar-widget' ); ?></option>
+					<option value="newest" <?php selected( $type, 'newest' ); ?> ><?php _e( 'Recently Joined', 'bp-members-with-uploaded-avatar-widget' ); ?></option>
+					<option value="popular" <?php selected( $type, 'popular' ); ?> ><?php _e( 'Popularity', 'bp-members-with-uploaded-avatar-widget' ); ?></option>
 				</select>
-
 			</label>
 		</p>
 		<p>
@@ -120,30 +117,27 @@ class BP_Members_With_Uploaded_Avatar_Widget extends WP_Widget {
 				<input type='checkbox' name="<?php echo $this->get_field_name( 'avatar_option' ); ?>"
 				       id="<?php echo $this->get_field_id( 'avatar_option' ); ?>"
 				       value="1" <?php echo checked( 1, $avatar_option ); ?> />
-				<?php _e( 'Show Members without avatars too?' ); ?>
+				<?php _e( 'Show Members without avatars too?', 'bp-members-with-uploaded-avatar-widget' ); ?>
 			</label>
 		</p>
 		<p>
-			<label for="bp-member-with-avatar-size"><?php _e( 'Avatar Size' ); ?>
+			<label for="bp-member-with-avatar-size"><?php _e( 'Avatar Size', 'bp-members-with-uploaded-avatar-widget' ); ?>
 				<select class="widefat" id="<?php echo $this->get_field_id( 'size' ); ?>"
 				        name="<?php echo $this->get_field_name( 'size' ); ?>" style="width: 30%">
-					<option value="full" <?php selected( $size, 'full' ); ?> >Full</option>
-					<option value="thumb" <?php selected( $size, 'thumb' ); ?> >Thumb</option>
-
-
+					<option value="full" <?php selected( $size, 'full' ); ?> ><?php _e( 'Full', 'bp-members-with-uploaded-avatar-widget' );?></option>
+					<option value="thumb" <?php selected( $size, 'thumb' ); ?> ><?php _e( 'Thumb', 'bp-members-with-uploaded-avatar-widget' );?></option>
 				</select>
-
 			</label>
 		</p>
 		<p>
-			<label for="bp-member-with-avatar-height"><?php _e( 'Avatar height' ); ?>
+			<label for="bp-member-with-avatar-height"><?php _e( 'Avatar height', 'bp-members-with-uploaded-avatar-widget' ); ?>
 				<input class="widefat" id="<?php echo $this->get_field_id( 'height' ); ?>"
 				       name="<?php echo $this->get_field_name( 'height' ); ?>" type="text"
 				       value="<?php echo esc_attr( $height ); ?>" style="width: 30%"/>
 			</label>
 		</p>
 		<p>
-			<label for="bp-member-with-avatar-width"><?php _e( 'Avatar Widtht' ); ?>
+			<label for="bp-member-with-avatar-width"><?php _e( 'Avatar Widtht', 'bp-members-with-uploaded-avatar-widget' ); ?>
 				<input class="widefat" id="<?php echo $this->get_field_id( 'width' ); ?>"
 				       name="<?php echo $this->get_field_name( 'width' ); ?>" type="text"
 				       value="<?php echo esc_attr( $width ); ?>" style="width: 30%"/>
